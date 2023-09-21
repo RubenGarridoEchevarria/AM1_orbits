9d4a2e819e9550166396e3009fe5fb823c0812fc
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-09-21 17:59:03 +0200

    act

--- Pruebas/Semana1.py
+++ Pruebas/Semana1.py
@@ -14,7 +14,7 @@ def F_Kepler(U):
    
    return array( [Vx , Vy , -x/mr, -y/mr] )
 
-N = 10000
+N = 100
 print(N)
 U = array([1,0,0,1])
 dt = 0.01
@@ -28,7 +28,7 @@ y[0]= U[0]
 for i in range(0,N):
    
    F = array( [U[2], U[3] , -U[0]/(U[0]**2+U[1]**2)**1.5 , -U[1]/(U[0]**2+U[1]**2)**1.5] )
-   F=F_Kepler(U)  #El paradigma segundo puedo quitar la línea de arriba y va a funcionar igual.
+   #F=F_Kepler(U)  #El paradigma segundo puedo quitar la línea de arriba y va a funcionar igual.
    U = U + dt*F
    x[i] = U[0]
    y[i] = U[1]
