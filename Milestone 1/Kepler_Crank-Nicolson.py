0636db1391858c353fc4b3022003997db66f1218
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-09-24 18:22:33 +0200

    act

--- Milestone 1/Kepler_Crank-Nicolson.py
+++ Milestone 1/Kepler_Crank-Nicolson.py
@@ -5,7 +5,7 @@ from numpy import array, zeros
 import matplotlib.pyplot as plt
 
 u = array( [1, 0, 0, 1] )  #Condición inicial de la ecuación diferncial
-n=3200                    #Número de pasos de tiempo
+n=6300                    #Número de pasos de tiempo
 dt=0.001                   #Paso de tiempo 
 
 x = array( zeros(n) )     #Posición x del vector de posición   
@@ -20,11 +20,11 @@ for i in range(0,n):
 
     # Para calcular la solución u en el tiempo siguiente con Crank-Nicolsón necesito F en el punto siguiente
 
-    u = u + dt * F_e 
+    u_e = u + dt * F_e 
 
     # Una vez calculada la solución con Euler vuelvo a calcular F con esa solución
 
-    F_CN = array( [ u[2], u[3], -u[0]/(u[0]**2+u[1]**2)**1.5 , -u[1]/( u[0]**2 + u[1]**2)**1.5] )
+    F_CN = array( [ u_e[2], u_e[3], -u_e[0]/(u_e[0]**2+u_e[1]**2)**1.5 , -u_e[1]/( u_e[0]**2 + u_e[1]**2)**1.5] )
 
     # Por último calculo la solución final del problema con la expresión de Crank-Nicolson 
 
