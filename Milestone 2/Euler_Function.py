7441e48279315ef08b6aa537a9cb2bb6bda393ce
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-10-02 18:03:22 +0200

    act

--- Milestone 2/Euler_Function.py
+++ Milestone 2/Euler_Function.py
@@ -1,1 +1,24 @@
 
+import matplotlib.pyplot as plt
+from numpy import array
+from numpy import linspace 
+
+import sys
+sys.path.append('/Users/ruben/Library/Mobile Documents/com~apple~CloudDocs/Wantanamo/Ampliación de Matemáticas I/GitHub/Sin título/Milestone 1')
+import Kepler_Euler
+
+
+
+def Euler(tf, N, U0):
+    
+    t = linspace(0,tf,N)
+
+
+
+
+
+
+
+
+
+    
