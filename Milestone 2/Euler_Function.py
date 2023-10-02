3815e14fd1509fc280be4c62af09a845e8a43be7
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-10-02 18:10:20 +0200

    act

---
+++ Milestone 2/Euler_Function.py
@@ -0,0 +1,21 @@
+
+import matplotlib.pyplot as plt
+from numpy import array
+from numpy import linspace 
+from Milestone 1.Kepler_Euler import F_Kepler
+
+
+
+def Euler(tf, N, U0):
+    
+    t = linspace(0,tf,N)
+
+
+
+
+
+
+
+
+
+    
