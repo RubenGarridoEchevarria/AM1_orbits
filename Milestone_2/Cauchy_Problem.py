45b588ff1bf3e8f78753d7ce672c8f57f489610b
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-10-02 18:56:54 +0200

    act

--- Milestone_2/Cauchy_Problem.py
+++ Milestone_2/Cauchy_Problem.py
@@ -1,8 +1,9 @@
 
 from numpy import zeros
 from numpy import array
+from Milestone_1.Kepler_Euler import Euler
 
-def Cauchy_problem(F,t, U0, Kepler_Euler):
+def Cauchy_problem(F,t, U0, Integrador_temporal):
     
     N = len(t)-1
     Nv = len(U0)
@@ -11,7 +12,7 @@ def Cauchy_problem(F,t, U0, Kepler_Euler):
 
     for i in range(N):
 
-        U[i+1,:] = Euler( U[i, :], t[i+1] - t[i], t[i],  F)
+        U[i+1,:] = Integrador_temporal( U[i, :], t[i+1] - t[i], t[i],  F)
 
 
     return U
--- Milestone_2/Euler_Function.py
+++ Milestone_2/Euler_Function.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from numpy import array
 from numpy import linspace 
 from Milestone_1.Kepler_Euler import Euler
+from Milestone_1.Kepler_Euler import F_Kepler
+import Cauchy_Problem
 
 
 
@@ -11,7 +13,7 @@ def Euler(tf, N, U0):
     t = linspace(0,tf,N)
 
     temporal_integrator = Euler
-    U
+    U = Cauchy_Problem(F_Kepler, t, U0, Euler)
 
 
 
