392be7b0bab1fb14338a7aa417ab4bd6581b57fe
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-10-04 11:10:19 +0200

    act

--- Milestone_2/Euler_Function.py
+++ Milestone_2/Euler_Function.py
@@ -17,13 +17,13 @@ def Euler_function(tf, N, U0):
     t = linspace(0,tf,N)
 
     temporal_integrator = Kepler_Euler.Euler
-    U = Cauchy_Problem(Kepler_Euler.F_Kepler, t, U0, temporal_integrator)
+    U = Cauchy_Problem(Kepler_Euler.F_Kepler, t, U0, temporal_integrator=Euler)
     plt.plot( U[:,0] , U[:,1], "." )
     plt.show()
 
 
 
-Euler_function(100,100, array([1,0,0,1]))
+Euler_function(tf=100,N=100, U0=array([1,0,0,1]))
 
 
 
