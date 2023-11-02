9b7103076eac8b31b8df4c6822bebfbe8d9757b7
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-11-02 13:12:09 +0100

    act

---
+++ Pruebas/Stabililty.py
@@ -0,0 +1,16 @@
+
+from numpy import array,zeros, float64
+
+def System_matrix(F , U0 ; t):
+
+    eps = 1e-6
+    N=len(U0)
+    A=zeros( (N,N), dtype=float64 )
+    delta=zeros(N)
+
+
+    for j in range(N)
+        
+        delta[:]=0
+        delta[j]=eps
+        A[:,j]=(F(U0+delta,t) - F)
