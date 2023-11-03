d0eaccf2f536cecd20592adf9fa3c2f79356aa87
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-11-03 11:49:01 +0100

    act

--- Milestone_3/Region_estabilidad.py
+++ Milestone_3/Region_estabilidad.py
@@ -1,32 +1,36 @@
-from numpy import array, zeros, linspace ,abs,traspose, float64
+from numpy import array, zeros, linspace ,abs,transpose, float64
 import matplotlib.pyplot as plt
 from Temporal_integrator import Euler,RK4,Crank_Nicolson
 
 
-def Stability_Region(Scheme,N,x0,xf,y0,yf):
+def Stability_Region( Scheme , N , x0 , xf , y0 , yf):
     
-    x, y=linspace(x0,xf,N), linspace(y0,yf,N)
+    x, y = linspace( x0 , xf , N ), linspace( y0 , yf , N )
 
-    rho=zeros(N,N)
+    rho = zeros((N , N), dtype=float64)
 
-    for i in range(N):
-        for j in range(N):
+    for i in range( N ):
+        for j in range( N ):
 
 
             w = complex(x[i],y[j])
-            r = Scheme(1.,1.,0., lambda u, t:w*u)   #Lambda crea una función como def pero sin nombre
+            r = Scheme( 1., 1. , 0., lambda u, t:w*u)   #Lambda crea una función como def pero sin nombre
             rho[i,j]=abs(r)
 
+    return rho, x, y  
 
 def test_Stability_region():
 
-    for schemes in schemes:
-        rho,x,y= Stability_Region(Scheme,100,-4,2,-4,4)
-        plt.contour(x,y, traspose(rho), linspace(0,1,11))
+    schemes = [RK4, Euler, Crank_Nicolson]
+
+    for scheme in schemes:
+
+        rho, x, y = Stability_Region (scheme,100,-4,2,-4,4)
+       
+        plt.contour(x,y, transpose(rho), linspace(0,1,11))
         plt.axis("equal")
         plt.grid()
         plt.show()
 
 
-if __name__ == "__main__":      #Se ejecuta si este es el programa principal
-    test_Stability_region()
+test_Stability_region()
