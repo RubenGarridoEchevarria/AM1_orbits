a416c394696a5bfc49837434e52881c2703e3589
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-11-07 10:09:27 +0100

    act
    act

--- Pruebas/Orbitas.py
+++ Pruebas/Orbitas.py
@@ -9,20 +9,21 @@ import matplotlib.pyplot as plt
 
 #Condición inicial
 
-ci=linspace(-5,1,5)
 
-U1 = array([2, 1])
 
-U2 = array([1, 2])
+U1 = array([1.75, 2.25])
 
-U3 = array([-1, -2])
+U2 = array([2, 3])
 
-U4 = array([-2, -1])
+U3 = array([1.3, 2.6])
+
+U4 = array([-2, 6])
 
 
 dt=0.01
 
 n=2000
+t=linspace(0,4,n)
 
 x1 = array( zeros(n) )     #Posición x del vector de posición   
 y1 = array( zeros(n))      #Posición y del vector de posición
@@ -54,10 +55,10 @@ y4[0] = U4[1]                 #Asigno el primer valor de la y del vector de posi
 
 for i in range(0,n):
 
-    F1=array([-3/2 * U1[0] + U1[1] , -1/4*U1[0] - 1/2*U1[1]])
-    F2=array([-3/2 * U2[0] + U2[1] , -1/4*U2[0] - 1/2*U2[1]])
-    F3=array([-3/2 * U3[0] + U3[1] , -1/4*U3[0] - 1/2*U3[1]])
-    F4=array([-3/2 * U4[0] + U4[1] , -1/4*U4[0] - 1/2*U4[1]])
+    F1=array([1-4*U1[0]+U1[0]**2*U1[1], 3*U1[0]-U1[0]**2*U1[1]])       #Meter la matriz del sistema de ecuaciones diferenciales con U[0] = x, U[1] = y
+    F2=array([1-4*U2[0]+U2[0]**2*U2[1], 3*U2[0]-U2[0]**2*U2[1]])
+    F3=array([1-4*U3[0]+U3[0]**2*U3[1], 3*U3[0]-U3[0]**2*U3[1]])
+    F4=array([1-4*U4[0]+U4[0]**2*U4[1], 3*U4[0]-U4[0]**2*U4[1]])
 
     U1 = U1 + dt * F1
     U2 = U2 + dt * F2
@@ -81,5 +82,15 @@ plt.plot(x1,y1)
 plt.plot(x2,y2)
 plt.plot(x3,y3)
 plt.plot(x4,y4)
+
+plt.grid()
+plt.show()
+plt.grid()
+
+
+plt.plot(t,x1)
+plt.plot(t,x2)
+plt.plot(t,x3)
+plt.plot(t,x4)
 
 plt.show()
