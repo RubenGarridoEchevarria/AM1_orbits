8956cf48bd5cf89eb014d8f2da83758439892d3a
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-11-22 18:57:15 +0100

    act

--- Milestone_5/F_N_Cuerpos.py
+++ Milestone_5/F_N_Cuerpos.py
@@ -1,7 +1,34 @@
 
+from numpy import reshape, zeros
+from numpy.linalg import norm
 
-
-#def F_N_Cuerpos(U,t, Nb, Nc):
+def F_N_Cuerpos(U,t, Nb, Nc):
+    
+    Us = reshape(U, (Nb, Nc, 2))     # Se convierte U en un tensor que contendrá la información de posición y velocidad de todos los cuerpos y sus coordenadas
+    F = zeros(len(U))                 
+    dUs = reshape(F, (Nb, Nc,2))    # Convierto la derivada (La función F) en un tensor de la misma manera
     
     
+    r = reshape(Us[:,:,0], (Nb, Nc))       # Posiciones de los cuerpos y sus coordenadas en una sola matriz
+    v = reshape(Us[:,:,1], (Nb, Nc))       # Velocidades de los cuerpos y sus coodenadas
     
+    
+    drdt = reshape(dUs[:,:,0], (Nb,Nc))   # Lo mismo con las derivadas
+    dvdt = reshape(dUs[:,:,1], (Nb,Nc)) 
+    
+    dvdt[:,:] = 0 
+    
+    for i in range(Nb):  #Bucle para cada cuerpo
+        
+        drdt[i,:] = v[i,:] # Relleno la derivada de la posición con de cada cuerpo para todas la coordenadas con la velocidad
+        
+        for j in range(Nb):
+            
+            if j !=i:
+                
+                d = r[j,:] - r[i,:]
+                dvdt[i,:] = dvdt[i,:] + d[:]/norm(d)**3  # La aceleración de cada cuerpo viene dado por la diferencia entre la posición de cada cuerpo partido del módulo de esa distancia
+                
+                
+    return F
+            
--- Milestone_5/Problema_N_Cuerpos.py
+++ Milestone_5/Problema_N_Cuerpos.py
@@ -1,4 +1,60 @@
 
 from Cauchy_Problem import Cauchy_problem
-from Temporal_integrator import Euler, RK4, Crank_Nicolson
 from numpy import array, zeros, linspace, reshape
+from F_N_Cuerpos import F_N_Cuerpos
+from Temporal_integrator import RK4
+import matplotlib.pyplot as plt
+
+
+def Condiciones_iniciales (Nc, Nb):
+    
+    U0 = zeros(2*Nc*Nb)     # Vector de condición inicial que contiene la información de posición y velocidad de cada para cada cuerpo y sus coordenadas
+    U1 = reshape(U0, (Nb,Nc,2))  # Convierto el vector en un tensor
+    r0 = reshape(U1[:,:,0],(Nb,Nc))  # Separo el tensor en posiciones y velocidades para cada cuerpo y sus cuerpos
+    v0 = reshape(U1[:,:,1],(Nb,Nc))
+    
+    
+    r0[0,:] = [1, 0, 0] # Posición inicial del cuerpo 1 para todas la coordenadas tienen componentes en los tres ejes
+    v0[0,:] = [0, 0.4,0]
+    
+    r0[1,:] = [-1, 0, 0] # Posición inicial del cuerpo 2 para todas la coordenadas tienen componentes en los tres ejes
+    v0[1,:] = [0, -0.4,0]
+    
+    r0[2,:] = [0, 1, 0] # Posición inicial del cuerpo 3 para todas la coordenadas tienen componentes en los tres ejes
+    v0[2,:] = [-0.4, 0,0]
+    
+    r0[3,:] = [0, -1, 0] # Posición inicial del cuerpo 4 para todas la coordenadas tienen componentes en los tres ejes
+    v0[3,:] = [0.4, 0,0]
+    
+    return U0
+
+
+def Milestone_5():
+    
+    def F(U,t):
+        
+        return F_N_Cuerpos(U,t, Nb,Nc)
+    
+    N = 1000
+    Nb = 4      # Número de cuerpos
+    Nc = 3      # Componentes de las coordenadas
+    t0 = 0
+    tf = 4 * 3.14
+    t = linspace(t0,tf, N+1)
+    
+    U0 = Condiciones_iniciales(Nc, Nb)
+    U = Cauchy_problem(t, U0, RK4, F)
+    
+    Us = reshape(U, (N+1, Nb, Nc, 2))
+    r = reshape(Us[:,:,:,0], (N+1, Nb, Nc))
+    
+    for i in range(Nb):
+        
+        plt.plot (r[:,i, 0], r[:,i,1])
+        
+    plt.grid()
+    plt.show()
+    
+    
+Milestone_5()
+
