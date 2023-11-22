b1cc42f1817ab0c4ebab5e1ac5da9ce2bf9c9f50
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-11-22 19:00:30 +0100

    act

--- Milestone_5/Problema_N_Cuerpos.py
+++ Milestone_5/Problema_N_Cuerpos.py
@@ -4,6 +4,7 @@ from numpy import array, zeros, linspace, reshape
 from F_N_Cuerpos import F_N_Cuerpos
 from Temporal_integrator import RK4
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def Condiciones_iniciales (Nc, Nb):
@@ -18,13 +19,13 @@ def Condiciones_iniciales (Nc, Nb):
     v0[0,:] = [0, 0.4,0]
     
     r0[1,:] = [-1, 0, 0] # Posición inicial del cuerpo 2 para todas la coordenadas tienen componentes en los tres ejes
-    v0[1,:] = [0, -0.4,0]
+    v0[1,:] = [2, -0.4,0]
     
     r0[2,:] = [0, 1, 0] # Posición inicial del cuerpo 3 para todas la coordenadas tienen componentes en los tres ejes
     v0[2,:] = [-0.4, 0,0]
     
     r0[3,:] = [0, -1, 0] # Posición inicial del cuerpo 4 para todas la coordenadas tienen componentes en los tres ejes
-    v0[3,:] = [0.4, 0,0]
+    v0[3,:] = [0.4, 0,1]
     
     return U0
 
@@ -39,7 +40,7 @@ def Milestone_5():
     Nb = 4      # Número de cuerpos
     Nc = 3      # Componentes de las coordenadas
     t0 = 0
-    tf = 4 * 3.14
+    tf = 4 
     t = linspace(t0,tf, N+1)
     
     U0 = Condiciones_iniciales(Nc, Nb)
@@ -48,9 +49,11 @@ def Milestone_5():
     Us = reshape(U, (N+1, Nb, Nc, 2))
     r = reshape(Us[:,:,:,0], (N+1, Nb, Nc))
     
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
     for i in range(Nb):
         
-        plt.plot (r[:,i, 0], r[:,i,1])
+        plt.plot (r[:,i, 0], r[:,i,1], r[:,i,2] ) # Pinto solo las coordenadas x e y
         
     plt.grid()
     plt.show()
