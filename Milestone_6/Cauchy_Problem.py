9bcd1353c4ce3030ea801b27397f07ca75c50bb3
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-12-16 12:47:52 +0100

    act
    act

--- Milestone_6/Cauchy_Problem.py
+++ Milestone_6/Cauchy_Problem.py
@@ -17,7 +17,7 @@ def Cauchy_problem(t, U0, Temporal_integrator, F):  #Defino el problema de Cauch
     for i in range(0,N):          #Crea el vector del problema de Cauchy para luego en cada paso del bucle resolverlo
 
     
-            U[i+1,:] = Temporal_integrator(U[i, :] , t[i+1] - t[i], t[i],  F) #Crea el vector problema de Cauchy en el siguiente paso de tiempo y llama al integrador temporal para que lo resulva
+            U[i+1,:] = Temporal_integrator(U[i, :] , t[i+1] - t[i], t[i],  F,8,1e-7) #Crea el vector problema de Cauchy en el siguiente paso de tiempo y llama al integrador temporal para que lo resulva
 
 
     return U    #Devuelve la imagen de lo calculado por el integrador temporal
