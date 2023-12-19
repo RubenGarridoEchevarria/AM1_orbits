cf17aea846e5775f7986e3e44969a7e0ec397c0b
RubenGarridoEchevarria <144596288+RubenGarridoEchevarria@users.noreply.github.com>
2023-12-19 19:56:20 +0100

    act

--- Milestone_6.py
+++ Milestone_6.py
@@ -3,11 +3,11 @@ from Funciones_Auxiliares.RK_Embebido import Embedded_RK
 
 from scipy.optimize import newton
 from scipy.integrate import solve_ivp
-from Funciones_Auxiliares.Temporal_integrator import RK4
+from Funciones_Auxiliares.Temporal_integrator import RK4, Crank_Nicolson
 from Funciones_Auxiliares.F_3_Cuerpos import F_3_Cuerpos
 
 from Funciones_Auxiliares.Cauchy_Problem_Con_F import Cauchy_problem
-from numpy import sqrt, array, hstack, cos, sin, abs, concatenate, linspace, pi, zeros_like, sum, square
+from numpy import sqrt, array, hstack, cos, sin, abs, concatenate, linspace, pi, zeros_like, sum, square, float64
 import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 
@@ -18,28 +18,31 @@ def Problema_Tres_Cuerpos_Restirngidos():
     
     #Condiciones iniciales
 
-    m_1 = 5.974E24 
-    m_2 = 7.348E22 
+    m_1 = 5.974E24                      # Masa de la tierra
+    m_2 = 7.348E22                      # Masa de la luna
     pi_2 = m_2/(m_1 + m_2)              # Relación de la segunda masa entre la masa total
-
+    
+    
+ ## Seleccionar la condición inicila de los diferentes puntos de Lagrange calculados con la última función
+ # Todas las cordenadas están adimensionalizadas respecto a la distancia entre la tierra y la luna
  
    # Condiciones iniciales para el L1
     
-    x_0 = 0.8369154703225321                  
-    y_0 = 0
-    z_0 = 0
-    vx_0 = 0
-    vy_0 = 0
-    vz_0 = 0
-    
-    # Condiciones iniciales para el L2
-    
-    # x_0 = 1.1556818961296604                
+    # x_0 =  0.8369154703220377
     # y_0 = 0
     # z_0 = 0
     # vx_0 = 0
     # vy_0 = 0
     # vz_0 = 0
+    
+    # Condiciones iniciales para el L2
+    
+    x_0 = 1.1556818961291992      
+    y_0 = 0
+    z_0 = 0
+    vx_0 = 0
+    vy_0 = 0
+    vz_0 = 0
     
     # Condiciones iniciales para el L3
     
@@ -51,36 +54,56 @@ def Problema_Tres_Cuerpos_Restirngidos():
     # vz_0 = 0
     
     
+    # # Condiciones iniciales para el L4
+    
+    # x_0 = 0.5 - pi_2         
+    # y_0 = sqrt(3)/2
+    # z_0 = 0
+    # vx_0 = 0
+    # vy_0 = 0
+    # vz_0 = 0
+    
+    #  # Condiciones iniciales para el L5
+    
+    # x_0 = 0.5 - pi_2         
+    # y_0 = -sqrt(3)/2
+    # z_0 = 0
+    # vx_0 = 0
+    # vy_0 = 0
+    # vz_0 = 0
+    
+  
+    
+    
 
-    r_0 = array((x_0, y_0, z_0))
-    v_0 = array((vx_0, vy_0, vz_0))
-    U_0 = hstack((r_0, v_0))
+    r_0 = array((x_0, y_0, z_0))                # Posiciones iniciales
+    v_0 = array((vx_0, vy_0, vz_0))             # Velocidades iniciales
+    U_0 = hstack((r_0, v_0))                    # Matriz de posiciones y velocidades iniciales
 
-    t_0 = 0  
-    t_f = 100  
-    t_points = linspace(t_0, t_f, 1000)
+    t_0 = 0                                     # Tiempo inicial 
+    t_f = 20                                  # Tiempo final
+    t_points = linspace(t_0, t_f, 1000)         
     
     
     
     # Resolución del problema de los 3 cuerpos resitringidos
     
-    U = Cauchy_problem(t_points, U_0, RK4, F_3_Cuerpos)
+    U = Cauchy_problem(t_points, U_0, RK4, F_3_Cuerpos)         # Resolución del porblema de los tres cuerpos llamando a la funcion Cauchy Porblem. Esta a su vez usará la función RK4 como integrador temporal
 
-    r = U[:, :3]    # Posicion
-    v = U[:, 3:]    # Velocidad
-
-
+    r = U[:, :3]    # Vector de posicion
+    v = U[:, 3:]    # Vector de velocidad
     x_2 = (1 - pi_2) * cos(linspace(0, pi, 100))
     y_2 = (1 - pi_2) * sin(linspace(0, pi, 100))
+
+
     fig, ax = plt.subplots(figsize=(5,5), dpi=96)
 
-    
-    ax.plot(r[:, 0], r[:, 1], 'r', label="Trajectory")
+    ax.plot(r[:, 0], r[:, 1], 'k', label="Trajectory")
     ax.axhline(0, color='k')
     ax.plot(hstack((x_2, x_2[::-1])), hstack((y_2, -y_2[::-1])))
-    ax.plot(-pi_2, 0, 'bo', label="$m_1$")
-    ax.plot(1 - pi_2, 0, 'go', label="$m_2$")
-    ax.plot(x_0, y_0, 'ro')
+    ax.plot(-pi_2, 0, 'bo', label="$m_1$")              # Posición de la tierra
+    ax.plot(1 - pi_2, 0, 'go', label="$m_2$")           # Posición de la luna
+    ax.plot(x_0, y_0, 'ro')                             # Satélite
     ax.set_aspect("equal")
 
     plt.show()
@@ -118,31 +141,11 @@ def Lagrange_Points():
         third_term = pi_2 / abs(xstar - 1 + pi_2)**3 * (xstar - 1 + pi_2)
         return first_term - second_term - third_term
     
-    L_2 = newton(func=collinear_lagrange, x0=1, args=(pi_2,))
-    L_1 = newton(func=collinear_lagrange, x0=0, args=(pi_2,))
+    L_2 = newton(func=collinear_lagrange, x0 = float64(1.0), args=(pi_2,))
+    L_1 = newton(func=collinear_lagrange, x0=0.836915, args=(pi_2,))
     L_3 = newton(func=collinear_lagrange, x0=-1, args=(pi_2,))
     print(f"{L_1=}, {L_2=}, {L_3=}")
-    fig, ax = plt.subplots(figsize=(5,5), dpi=96)
-    ax.set_xlabel("$x^*$")
-    ax.set_ylabel("$y^*$")
-
-  
-    ax.axhline(0, color='k')
-    ax.plot(hstack((x_2, x_2[::-1])), hstack((y_2, -y_2[::-1])))
-    ax.plot(hstack((x_1, x_1[::-1])), hstack((y_1, -y_1[::-1])))
-    ax.plot([-pi_2, 0.5 - pi_2, 1 - pi_2, 0.5 - pi_2, -pi_2], [0, sqrt(3)/2, 0, -sqrt(3)/2, 0], 'k', ls="--", lw=1)
-
     
-    ax.plot(L_1, 0, 'rv', label="$L_1$")
-    ax.plot(L_2, 0, 'r^', label="$L_2$")
-    ax.plot(L_3, 0, 'rp', label="$L_3$")
-    ax.plot(0.5 - pi_2, sqrt(3)/2, 'rX', label="$L_4$")
-    ax.plot(0.5 - pi_2, -sqrt(3)/2, 'rs', label="$L_5$")
-    ax.plot(0, 0, 'k', marker=center_of_mass, markersize=10)
-    ax.plot(-pi_2, 0, 'bo', label="$m_1$")
-    ax.plot(1 - pi_2, 0, 'go', label="$m_2$")
-    ax.legend()
-    ax.set_aspect("equal")
-    #plt.show()
         
 Lagrange_Points()    
+
